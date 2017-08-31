ca4fba6f030caba150802f21ca4829f7d4c95641
Vatsal Kanakiya <vatsal.kanakiyia@somaiya.edu>
2017-08-31 14:28:54 +0530

    Search Component written

---
+++ src/app/music/music-search/music-search.component.ts
@@ -0,0 +1,27 @@
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+
+@Component({
+	// Define Component
+	selector: 'music-search',
+	templateurl: './music-search.component.html',
+	styleUrls: ['./music-search.component.css']
+})
+
+export class MusicSearchComponent {
+	// Write Class of Component
+	track: string;	// Track variable of type string
+	@Input() tracks: any[];	// Tracks array of any datatype
+
+	@Output() update = new EventEmitter();	// update EventEmitter
+	@Output() query = new EventEmitter();	// query EventEmitter
+
+	search(event) {
+		// Function called when primeNG autocomplete is updated
+		this.query.emit(event.query);
+	}
+
+	select(track) {
+		// Function called when primeNG autocomplete item is selected
+		this.update.emit(track);
+	}
+}
---
+++ src/app/music/music.module.ts
@@ -0,0 +1,50 @@
+// Third party imports
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
+// PrimeNG autocomplete for search
+import { AutoCompleteModule } from 'primeng/primeng';
+
+// Custom imports
+import { MusicSearchComponent } from './music-search/music-search.component';
+import { MusicPlayerComponent } from './music-player/music-player.component';
+import { MusicDetailsComponent } from './music-details/music-details.component';
+import { MusicProgressComponent } from './music-progress/music-progress.component';
+import { MusicFooterCoeponent } from './music-footer/music-footer.component';
+import { MusicService } from './shared/music.service';
+import { ApiService } from './shared/api.service';
+
+@NgModule({
+	imports: [
+		// Define imports
+		FormsModule,
+		AutoCompleteModule,
+		HttpModule,
+		CommonModule
+	],
+	exports: [
+		// Expose components
+		MusicSearchComponent,
+		MusicDetailsComponent,
+		MusicPlayerComponent,
+		MusicProgressComponent,
+		MusicFooterComponent
+	],
+	declarations: [
+		// Declare components
+		MusicSearchComponent,
+		MusicDetailsComponent,
+		MusicPlayerComponent,
+		MusicProgressComponent,
+		MusicFooterComponent
+	],
+	providers: [
+		// Services
+		ApiService,
+		MusicService
+	],
+})
+
+export class MusicModule { }
+
